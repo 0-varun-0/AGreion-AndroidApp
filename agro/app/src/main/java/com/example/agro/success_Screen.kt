59384054b823a57f936ded4925c9977b55f5317c
0-varun-0 <115569823+0-varun-0@users.noreply.github.com>
2023-07-12 12:30:36 +0530

    success activity added

--- agro/app/src/main/java/com/example/agro/success_Screen.kt
+++ agro/app/src/main/java/com/example/agro/success_Screen.kt
@@ -1,15 +1,32 @@
 package com.example.agro
 
+import android.media.Image
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.semantics.Role.Companion.Image
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.agro.ui.theme.AgroTheme
 
 class success_Screen : ComponentActivity() {
@@ -22,7 +39,7 @@ class success_Screen : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting2("Android")
+                    Greeting2()
                 }
             }
         }
@@ -30,17 +47,34 @@ class success_Screen : ComponentActivity() {
 }
 
 @Composable
-fun Greeting2(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Greeting2( modifier: Modifier = Modifier) {
+    val image = painterResource(id = R.drawable.done)
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = CenterHorizontally
+    ) {
+
+        Image(painter = image, contentDescription = null)
+        Text(
+            text = "All Tasks Completed!",
+            modifier = modifier
+        )
+
+        Text(
+            text = "Nice Work",
+            modifier = modifier
+        )
+    }
 }
 
-@Preview(showBackground = true)
+
+@Preview(showBackground = true , showSystemUi = true)
 @Composable
 fun GreetingPreview2() {
     AgroTheme {
-        Greeting2("Android")
+        Greeting2()
     }
 }
